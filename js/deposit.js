74358ddddbd4e2b304072aba6a9bc8ad79de7b7e
Rahmat Ullah <rahmatbd1533@gmail.com>
2023-02-13 02:07:05 +0600

    bank management system recreated

---
+++ js/deposit.js
@@ -0,0 +1,31 @@
+// step 1: Add event listener to the deposit button
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    // step 2: get the deposit amount from the deposit input field
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    //step 3: get the current deposit total
+    //for non-input(element other than input, textarea) use innerText to get the text
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousdepositTotalString = depositTotalElement.innerText;
+    const previousdepositTotal = parseFloat(previousdepositTotalString);
+
+    //step 4: adding the deposit field input money
+    const currentDepositTotal = previousdepositTotal + newDepositAmount;
+    //set the total balance of deposit 
+    depositTotalElement.innerText = currentDepositTotal;
+
+    //step 5: get balance current total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    //step 6: calculate current total balance
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    //set the balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+    //step 7: clear the deposit field
+    depositField.value = ('');
+})
